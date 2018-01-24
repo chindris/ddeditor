c68745f637e559123bff7b4fbb8f0eeee0bf2eca
Vasile Chindris <vasile.chindris@amazee.com>
2018-01-24 08:55:33 +0200

    Move the jsoncontent field formatter into the jsoncontent_entity module.

--- web/modules/custom/jsoncontent/modules/jsoncontent_entity/src/Plugin/Field/FieldFormatter/JsonContentEntityFormatter.php
+++ web/modules/custom/jsoncontent/modules/jsoncontent_entity/src/Plugin/Field/FieldFormatter/JsonContentEntityFormatter.php
@@ -1,25 +1,27 @@
 <?php
 
-namespace Drupal\jsoncontent\Plugin\Field\FieldFormatter;
+namespace Drupal\jsoncontent_entity\Plugin\Field\FieldFormatter;
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\jsoncontent\Plugin\Field\FieldFormatter\JsonContentFormatterBase;
 
 /**
- * Default formatter for the jsoncontent field type.
+ * Entity formatter for the jsoncontent field type.
  *
  * @FieldFormatter(
- *   id = "jsoncontent_formatter",
- *   label = @Translation("JSON Content default"),
+ *   id = "jsoncontent_entity_formatter",
+ *   label = @Translation("JSONContent Entity View"),
  *   field_types = {
  *     "jsoncontent"
  *   }
  * )
  */
-class JsonContentFormatter extends JsonContentFormatterBase {
+class JsonContentEntityFormatter extends JsonContentFormatterBase {
 
   public function viewFieldElements(FieldItemListInterface $items, $langcode) {
     $element = [];
     foreach ($items as $delta => $item) {
       foreach ($item->value as $entity_meta) {
+        // @todo: inject the entityTypeManager service.
         $render_controller = \Drupal::entityTypeManager()->getViewBuilder($entity_meta['entityType']);
         $entity = \Drupal::entityTypeManager()->getStorage($entity_meta['entityType'])->create($entity_meta);
         $element[$delta][] = $render_controller->view($entity, 'full', $langcode);
